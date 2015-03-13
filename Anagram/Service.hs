61cc7023a36fad0e1a006874b2570649eb366e18
Jochen Keil <jochen.keil@gmail.com>
2015-03-13 14:25:20 +0100

    Add CORS support

--- Anagram/Service.hs
+++ Anagram/Service.hs
@@ -7,6 +7,7 @@ import Control.Applicative
 import Anagram.Config
 import Anagram.Types
 import Anagram.Solver
+import Network.Wai.Middleware.Cors
 import Network.Wai.Handler.Warp (Port)
 
 service :: Port -> IO ()
@@ -14,5 +15,6 @@ service p = buildTable . words <$> readFile defaultDictionary >>= runScotty p
 
 runScotty :: Port -> AnagramTable -> IO ()
 runScotty port table = scotty port $ do
+    middleware simpleCors
     get (capture "/:word") $ flip findAnagrams table <$> param "word" >>= json
     notFound               $ json $ Error 400 "service not found" Nothing
