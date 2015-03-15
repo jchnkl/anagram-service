3d04aa556750a1954e7eeb1c6d312f13e4c51753
Jochen Keil <jochen.keil@gmail.com>
2015-03-15 15:00:36 +0100

    Cosmetic: Sort imports

--- Anagram/Service.hs
+++ Anagram/Service.hs
@@ -2,14 +2,14 @@
 
 module Anagram.Service (service) where
 
-import Web.Scotty
-import Data.Text.Lazy (unpack)
-import Control.Applicative
 import Anagram.Config
-import Anagram.Types
 import Anagram.Solver
-import Network.Wai.Middleware.Cors
+import Anagram.Types
+import Control.Applicative
+import Data.Text.Lazy (unpack)
 import Network.Wai.Handler.Warp (Port)
+import Network.Wai.Middleware.Cors
+import Web.Scotty
 
 service :: Port -> IO ()
 service p = buildTable . words <$> readFile defaultDictionary >>= runScotty p
